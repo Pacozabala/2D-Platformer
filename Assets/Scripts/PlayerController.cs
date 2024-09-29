38c5a8f0ea84f418c1890773627fe02155ad003f
LCBT <cedric.tan@student.ateneo.edu>
2024-09-30 05:53:16 +0800

    Fixed platforms and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,13 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public BoxCollider2D footCollider, bodyCollider, headCollider;
-    public bool grounded;
-    public float xSpeed, ySpeed, xInput, yInput, groundFriction, gravity;
+    public bool grounded, jumpPressed;
+    public float xSpeed, ySpeed, xInput, groundFriction, gravity;
     public LayerMask solidObjects;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        xSpeed = 5f;
-        ySpeed = 25f;
-        groundFriction = 0.9f;
-        gravity = 1f;
     }
 
     // Update is called once per frame
@@ -34,20 +30,20 @@ public class PlayerController : MonoBehaviour
 
     private void GetInputs() {
         xInput = Input.GetAxis("Horizontal");
-        yInput = Input.GetAxis("Vertical");
+        jumpPressed = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W);
     }
 
     private void MovePlayer() {
         if (Mathf.Abs(xInput) > 0) {
             rb.velocity = new Vector2(xInput * xSpeed, rb.velocity.y);
         }
-        if (Mathf.Abs(yInput) > 0 && grounded) {
+        if (jumpPressed && grounded) {
             rb.velocity = new Vector2(rb.velocity.x, ySpeed);
         }
     }
 
     private void CheckCollisions() {
-        bool groundCollided = Physics2D.BoxCast(footCollider.bounds.center, footCollider.size, 0, Vector2.down, 0.05f);
+        bool groundCollided = Physics2D.BoxCast(footCollider.bounds.center, footCollider.size, 0, Vector2.down, 0.1f);
         bool ceilingCollided = Physics2D.BoxCast(headCollider.bounds.center, headCollider.size, 0, Vector2.up, 0.05f, solidObjects);
 
         if (ceilingCollided) {
